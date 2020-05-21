2404b9af7ed15238aa04ad139d615a08b87ccae8
kenmaready <ken@maready.com>
2020-05-21 08:04:49 -0400

    fix: configuring initial db setup to work locally and on heroku

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
 # funcster\backend\app.py
-
 import os
-from flask import Flask, request, jsonify, abort
-import json
+from flask import Flask, jsonify
 from flask_cors import CORS
 
-#from models import db_drop_and_create_all, setup_db, Drink
-#from auth import AuthError, requires_auth
+from models import setup_db
 
-app = Flask(__name__)
-#setup_db(app)
-CORS(app)
+def create_app(test_config=None):
 
-@app.route('/')
-def index():
-  return jsonify({
-    "success": True,
-    "message": "welcome to funcster."
-  })
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
 
+    @app.route('/')
+    def index():
+      return jsonify({
+        "success": True,
+        "message": "welcome to funcster."
+      })
+
+    @app.route('/code', methods=['POST'])
+    def add_new_snippet():
+      body = request.get_json()
+      snippet = body.get('snippet', None)
+      snippet_type = body.get('snippet_type', None)
+
+    return app
+
+app = create_app()
 
 if __name__ == '__main__':
   app.run(port=5000)
---
+++ models.py
@@ -0,0 +1,23 @@
+import json
+import os
+
+from sqlalchemy import Column, String, create_engine
+from flask_sqlalchemy import SQLAlchemy
+
+
+database_path = os.environ['DATABASE_URL']
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
