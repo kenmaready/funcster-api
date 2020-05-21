0e6db625dc44a3e91adef3f042eaf343873fa762
kenmaready <ken@maready.com>
2020-05-21 16:51:53 -0400

    feat: first commit after connecting route to database models (testing if it works on heroku)

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # funcster\backend\app.py
 import os
-from flask import Flask, jsonify
+from flask import Flask, abort, jsonify, request
 
 from app_config import app
-from models import db, Mentor, Coder, Snippet
+from models import db, Mentor, Coder, Snippet, User
 
 
 @app.route('/')
@@ -12,6 +12,48 @@ def index():
     "success": True,
     "message": "welcome to funcster."
     })
+
+# route for new user signup. requires a username, email address and a status of mentor/coder
+@app.route('/signup', methods=['POST'])
+def signup_user():
+
+    # get body of request; if nothing included, abort with 400
+    body = request.get_json()
+    if not body:
+        abort(400)
+
+    # set attributes for new User based on body input
+    username = body.get('username', None)
+    usertype = body.get('usertype', None)
+
+    # proceed only if both required fields are provided
+    if username and usertype:
+
+        # first check to see if username is already being used, and abort with 409 (conflict) if so
+        if Mentor.exists(username) or Coder.exists(username):
+            abort(409)
+
+        # instantiate a new object for the new user
+        if usertype == 'mentor':
+            user = Mentor(username = username)
+        if usertype == 'coder':
+            user = Coder(username = username)
+        
+        # try to add the new user to the applicable table in the database
+        try:
+            user.insert()
+        except:
+            abort(500)
+    
+    # if required fields weren't provided in the request, abort with 400
+    else:
+        abort(400)
+    
+    return jsonify({
+        "success": "True",
+        "user_id": user.id
+    })
+
 
 @app.route('/code', methods=['POST'])
 def add_new_snippet():
--- models.py
+++ models.py
@@ -10,11 +10,60 @@ db = SQLAlchemy(app)
 
 '''
 User - generic User base class (can be further modelled into Mentor or Coder)
+     - used to avoid duplication of methods, as well as to require unique username at user level
 '''
 class User(db.Model):
     __abstract__ = True
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(24), unique=True)
+
+    '''
+    insert()
+        inserts a new user into a database
+        user must have a unique username
+        user must have a unique id or null id
+        EXAMPLE
+            mentor = Mentor(username = 'codeninja1')
+            mentor.insert()
+    '''
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    '''
+    delete()
+        deletes a user from a database
+        user must exist in the database
+        EXAMPLE
+            coder = Coder.query.get(13)
+            coder.delete()
+    '''
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    '''
+    update()
+        updates a user in a database
+        the user must exist in the database
+        EXAMPLE
+            mentor = Mentor.query.get(7)
+            mentor.username = 'codedevil23'
+            mentor.update()
+    '''
+    def update(self):
+        db.session.commit()
+
+    '''
+    exists(username)
+        checks to see whether a particular username already exists in the table
+        EXAMPLE
+            mentor.exists('codedevil23')
+    '''
+    @classmethod
+    def exists(cls, username):
+        return cls.query.filter_by(username=username).scalar() is not None
+
 
 '''
 Mentor - User who reviews code of Coders
@@ -42,7 +91,47 @@ Snippet - Code function or class written and stored by Coder and Reviewed by Men
 class Snippet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     snippet_name = db.Column(db.String(24))
+    code = db.Column(db.String())
+    code_type = db.Column(db.String())
     coder_id = db.Column(db.Integer, db.ForeignKey('coders.id'))
+
+    '''
+    insert()
+        inserts a new code snippet into a database
+        snippet must have a unique id or null id
+        snippet must have code and be associated with a coder
+        EXAMPLE
+            code = Code(snippet_name = 'addition', code='def add(x,y):\\n\\treturn (x+y)', code_type='function', coder_id=17)
+            code.insert()
+    '''
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    '''
+    delete()
+        deletes a code snippet from a database
+        snippet must exist in the database
+        EXAMPLE
+            snippet = Snippet.query.get(37)
+            snippet.delete()
+    '''
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    '''
+    update()
+        updates a code snippet in a database
+        the snippet must exist in the database
+        EXAMPLE
+            snippet = Snippet.query.get(21)
+            snippet.code = 'def add(x,y,z):\\n\\treturn (x+y+z)'
+            snippet.update()
+    '''
+    def update(self):
+        db.session.commit()
+
 
 
 db.create_all()
